320b010817daedb24cd8bdd08a97256b70963d28
Chirag Bansal <bansalchirag4043@gmail.com>
2025-01-23 12:20:01 +0530

    Solution to Perform Bitwise One's Complement

---
+++ Perform Bitwise One's Complement@@67882a731b4392d8fc1577e7/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    int a;
+    scanf("%d", &a);
+    int r = a ~ b;
+    printf("%d", r);
+    return 0;
+}
